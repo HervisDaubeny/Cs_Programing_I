d7a0b04d262799f452fcda14f3a92bc42d8d4515
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2019-10-21 08:08:17 +0200

    justify task created
    I' trying to crate soloution on my Windows system, get it on my Arch system via GIT and try to develop i in visual code.

---
+++ 02/block_justify/block_justify/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace block_justify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}

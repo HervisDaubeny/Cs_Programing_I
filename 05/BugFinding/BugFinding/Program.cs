16866f4f18077f4295afeb6d92ddd2892673d108
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2019-11-07 18:18:13 +0100

    Bug BugFinding completed.

---
+++ 05/BugFinding/BugFinding/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OHledaniBugu {
+    class Program {
+        static void Main( string[] args ) {
+
+            string line = Console.ReadLine();
+            if (line == null) {
+
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            bool parse = int.TryParse(line, out int a);
+            if (!parse || a < 0) {
+
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            line = Console.ReadLine();
+            if (line == null) {
+
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            parse = int.TryParse(line, out int b);
+            if (!parse || b < 0) {
+
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            int result = Math.Abs(a - b);
+
+            Console.WriteLine("Result: {0}", result);
+        }
+    }
+}

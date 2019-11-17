c359e492766bda594a49edb009cea94ba5d278c2
Sebastian Uhlík <47977130+HervisDaubeny@users.noreply.github.com>
2019-11-17 11:55:02 +0100

    Update: BinaryWriting tested and completed.

--- 05/BinaryWriting/BinaryWriting/Program.cs
+++ 05/BinaryWriting/BinaryWriting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,20 @@ using System.Threading.Tasks;
 namespace BinaryWriting {
     class Program {
         static void Main( string[] args ) {
+
+            Writer writer = new Writer(new FileStream(args[0], FileMode.OpenOrCreate, FileAccess.Write));
+
+            uint myBinaryNumber = 0b1000_0001;
+
+            Console.WriteLine("Output from Console.WriteLine():");
+            Console.WriteLine(myBinaryNumber);
+
+            Console.WriteLine("Output from BinaryWriter.Write()");
+            writer.WriteByte(myBinaryNumber);
+
+            writer.Dispose();
+
+            Console.ReadKey();
         }
     }
 }
